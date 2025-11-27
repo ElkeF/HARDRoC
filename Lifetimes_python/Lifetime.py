541f43dc79d31f55b0044d48270b57fc57bb222b
CatiaH <103994548+CatiaH@users.noreply.github.com>
2025-11-27 20:03:05 +0100

    Add: new function for Lifetime class
    Add function that calculates the lifetime for all SIPs

--- Lifetimes_python/Lifetime.py
+++ Lifetimes_python/Lifetime.py
@@ -1,4 +1,12 @@
 import numpy as np
 
 class Lifetime:
+
+  def gamma_comp():
+    """
+    calculates the lifetime (gamma) for all SIPs
+
+    gamma: lifetime of ICD for defined parameters in asymptotic approximation
+    """
+    return gamma
   
