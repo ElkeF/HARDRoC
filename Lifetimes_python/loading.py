9aa85467c797967c91a229ae8991772b2d006897
CatiaH <103994548+CatiaH@users.noreply.github.com>
2025-11-27 14:35:45 +0100

    Add: function for reading in SIPs

--- Lifetimes_python/loading.py
+++ Lifetimes_python/loading.py
@@ -3,3 +3,15 @@ import numpy as np
 import os
 import os.path
 import re
+
+def get_SIP(SIP_file):
+    #Reads in the Single Ionization Energies(SIP) from a file, 
+    #with energies in the first column and polestrengths in the
+    #second. Stores them in an array.
+
+    SIP_data = np.loadtxt(SIP_file)
+
+    SIP_energies = SIP_data[:,0] #[in eV]
+    SIP_polestrengths = SIP_data[:,1] #in a.u.
+
+    return SIP_energies
