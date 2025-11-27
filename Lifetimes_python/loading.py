20ac8a99b4dc77fb2bacfaeec68428ca41148be9
CatiaH <103994548+CatiaH@users.noreply.github.com>
2025-11-27 16:14:27 +0100

    revert: delete function to get DIP

--- Lifetimes_python/loading.py
+++ Lifetimes_python/loading.py
@@ -16,16 +16,3 @@ def get_SIP(SIP_file):
 
     return SIP_energies
 
-def get_DIP(DIP_file, SIP_file):
-    #Reading in the Doubleionisation energies (DIP) from a file,
-    #with energies in the first column and polestrengths in the second
-    #and returns only those energies which are smaller than the SIP
-    
-    DIP_data = np.loadtxt(DIP_file)
-
-    DIP_energies = DIP_data[:,0] #[in eV]
-    DIP_pole_strength = DIP_data[:,1] #in a.u.
-
-    filtered_DIP_energies = DIP_energies[DIP_energies < max(get_SIP(SIP_file))]
-
-    return filtered_DIP_energies
