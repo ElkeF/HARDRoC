9c9a17473a4129559dd08ee0ee0458eb0313482c
CatiaH <103994548+CatiaH@users.noreply.github.com>
2025-12-02 14:57:50 +0100

    Add: init function for Lifetime class

--- Lifetimes_python/Lifetime.py
+++ Lifetimes_python/Lifetime.py
@@ -2,6 +2,16 @@ import numpy as np
 
 class Lifetime:
 
+  def __init__(self, SIP_energies, DIP_energies, xs_dict, rec_tau, n_in_val_orbs, n_out_val_orbs):
+    """
+    :SIP_energies: the single ionization energies (in eV)
+    :DIP_energies: the double ionization energies (in eV)
+    :xs_dict: a dictionary with the photoionization cross-sections (in Mb) 
+    :rec_tau: an array with the reciprocal radiative lifetimes (in s**(-1))
+    :n_in_val_orbs: number of inner valence orbitals
+    :n_out_val_orbs: number of outer valence orbitals
+    """
+
   def gamma_comp():
     """
     calculates the lifetime (gamma) for all SIPs
