cb0af73bca7e541f078022c7e7b7d013a738fe39
CatiaH <103994548+CatiaH@users.noreply.github.com>
2025-12-02 14:51:20 +0100

    Add: function for lifetime for specific omega
    Add function that calculates the Lifetime for one specific decay channel with a corresponding omega

--- Lifetimes_python/Lifetime.py
+++ Lifetimes_python/Lifetime.py
@@ -9,4 +9,14 @@ class Lifetime:
     gamma: lifetime of ICD for defined parameters in asymptotic approximation
     """
     return gamma
+
+  def gamma_omega(self, omega, rec_tau_A, PI_xs_B):
+    """
+    calculates the lifetime for one specific omega
+
+    :omega: energy difference between final and initial state
+    :rec_tau_A: reciprocal radiative lifetime of the transition in monomer A
+    :PI_xs_B: photoionization cross-section of monomer B for the specified omega
+    """
+    return gamma_omega
   
